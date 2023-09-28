c873b8385bb9031326ee52918d149ce7e5284d45
Javi Vañó Bodí <javier.vano@payflow.es>
2023-09-28 14:04:01 +0200

    updating backend port

--- backend/config/default.js
+++ backend/config/default.js
@@ -3,7 +3,7 @@ module.exports = {
   loadedEnvironment: 'default-env',
   usePolling: true,
   botToken: undefined,
-  webServerPort: 3000,
+  webServerPort: 3333,
   frontendEndpoint: 'https://fardenz.github.io/telegram-mini-app/',
   mongodb: {
     user: 'root',
