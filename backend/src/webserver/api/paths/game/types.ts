eba353d0ea7424789b894ddf4e7adec06f5067fa
Javi Vañó Bodí <javier.vano@payflow.es>
2023-10-01 15:56:34 +0200

    game types

--- backend/src/webserver/api/paths/game/types.ts
+++ backend/src/webserver/api/paths/game/types.ts
@@ -2,6 +2,16 @@ import { ErrorResponse } from "../../../types"
 
 export type PostGameResult = PostGameResultSuccess | ErrorResponse
 
+export enum GameType{
+  DICE = 'dice',
+  COINFLIP = 'coinflip'
+}
+
+export interface PostGameBody {
+  choice: number
+  type: GameType
+}
+
 interface PostGameResultSuccess {
   result: number
 }
