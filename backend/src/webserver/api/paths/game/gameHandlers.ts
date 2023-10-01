142bf18bb13ed75eb77e0bbb7a9bd478c9562855
Javi Vañó Bodí <javier.vano@payflow.es>
2023-10-01 15:56:50 +0200

    adding coinflip and dice game

--- backend/src/webserver/api/paths/game/gameHandlers.ts
+++ backend/src/webserver/api/paths/game/gameHandlers.ts
@@ -2,8 +2,10 @@ import { injectable } from "inversify";
 import { Response } from "express"
 import { CustomError, CustomExpressRequest } from "../../../types";
 import User from "../../../../models/User";
-import { PostGameResult } from "./types";
+import { GameType, PostGameBody, PostGameResult } from "./types";
 import Decimal from "decimal.js";
+import { getCoinflipResult, getDiceResult } from "./helpers";
+import config from "../../../../config";
 
 @injectable()
 export default class GameEndpoints {
@@ -11,35 +13,55 @@ export default class GameEndpoints {
   constructor() {
   }
 
-  public async postGame(req: CustomExpressRequest, res: Response<PostGameResult>) {
-   try {
-    const user = await User.findOne({
-      telegramId: req.customData.telegramId
-    });
+  public async postGame(req: CustomExpressRequest<unknown, unknown, PostGameBody>, res: Response<PostGameResult>) {
+    try {
+      const user = await User.findOne({
+        telegramId: req.customData.telegramId
+      });
 
-    if (user === null || new Decimal(user?.walletAmount ?? 0).lessThan(1)) {
-      throw new CustomError("Not enough balance");
+      if (user === null || new Decimal(user?.walletAmount ?? 0).lessThan(1)) {
+        throw new CustomError("Not enough balance");
+      }
+
+      let result;
+      if (req.body.type === GameType.DICE) {
+        result = await getDiceResult()
+      } else if (req.body.type === GameType.COINFLIP) {
+        result = await getCoinflipResult()
+      } else {
+        throw new Error("Unrecognized game type");
+      }
+
+      await this.executeWalletIncrement(req.customData.telegramId, -1)
+
+      if (req.body.choice === result) {
+        // the user choice is correct
+        if (req.body.type === GameType.DICE) {
+          await this.executeWalletIncrement(req.customData.telegramId, new Decimal(6).mul(new Decimal(1).sub(config.houseEdge)).toNumber())
+        } else if (req.body.type === GameType.COINFLIP) {
+          await this.executeWalletIncrement(req.customData.telegramId, new Decimal(2).mul(new Decimal(1).sub(config.houseEdge)).toNumber())
+        }
+      }
+
+      res.send({
+        result
+      })
+    } catch (error) {
+      res.send({
+        error: 'Game could not run'
+      })
     }
+  }
 
-    const updatedUser = await User.findOneAndUpdate({
-      telegramId: req.customData.telegramId
+  private executeWalletIncrement(telegramId: string, amount: number): Promise<unknown> {
+    return User.findOneAndUpdate({
+      telegramId: telegramId
     }, {
       $inc: {
-        walletAmount: - 1
+        walletAmount: amount
       }
     }, {
       new: true
-    }).exec();
-
-
-
-    res.send({
-      result: 1
-    })
-   } catch (error) {
-    res.send({
-      error: 'Game could not run'
-    })
-   }
-  }
+    }).exec();}
+  
 }
