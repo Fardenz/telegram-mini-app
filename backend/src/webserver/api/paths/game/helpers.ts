f2cbf77dc21e14bb898ed90ca14faa7c5cac20ab
Javi Vañó Bodí <javier.vano@payflow.es>
2023-10-07 18:20:44 +0200

    fix random choice

--- backend/src/webserver/api/paths/game/helpers.ts
+++ backend/src/webserver/api/paths/game/helpers.ts
@@ -1,11 +1,11 @@
 import { randomInt } from "crypto";
 
 export function getDiceResult(): Promise<number> {
-  return randomIntPromisified(1, 6)
+  return randomIntPromisified(1, 7)
 }
 
 export function getCoinflipResult(): Promise<number> {
-  return randomIntPromisified(1, 2)
+  return randomIntPromisified(1, 3)
 }
 
 export const randomIntPromisified = function (min: number, max: number): Promise<number> {
