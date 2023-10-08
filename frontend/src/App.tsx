97d2a53c97e8405dfd4781a3b55d2e7ef1813430
Javi Vañó Bodí <javier.vano@payflow.es>
2023-10-08 21:01:44 +0200

    updating theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,13 @@
 import { RouterProvider } from "react-router-dom"
-import { ChakraProvider, extendTheme } from "@chakra-ui/react"
+import { ChakraProvider } from "@chakra-ui/react"
 import { Router } from "@router/router"
 import { TelegramContextProvider } from "@contexts/telegramContext"
-
-const theme = extendTheme({
-  colors: {
-    brand: {
-      100: "#ffffff", // lightest
-      200: "#30cccc",
-      300: "#30cccc",
-      400: "#ED2A4A",
-      500: "#ED2A4A",
-      600: "#33084F",
-      700: "#33084F",
-      800: "#33084F",
-      900: "#000000", // darkest
-    },
-  },
-})
+import { customTheme } from "./customTheme"
 
 const App: React.FC = () => {
   return (
     <TelegramContextProvider>
-      <ChakraProvider resetCSS theme={theme}>
+      <ChakraProvider resetCSS theme={customTheme}>
         <RouterProvider router={Router} />
       </ChakraProvider>
     </TelegramContextProvider>
